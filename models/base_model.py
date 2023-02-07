e3818f973e83fdebee970714c7b830eae1f293c0
Adeyemi Adejumo <adejumoadeyemi32@gmail.com>
2023-02-07 16:38:51 +0000

    pycodestyle check

--- models/base_model.py
+++ models/base_model.py
@@ -11,8 +11,10 @@ class BaseModel:
         """ constructor for the class
             Args:
                 id: (string) - assign with an uuid when an instance is created:
-                created_at: (datetime) - assign with the current datetime when an instance is created
-                updated_at: (datetime) - assign with the current datetime when an instance is created
+                created_at: (datetime) - assign with the current
+                datetime when an instance is created
+                updated_at: (datetime) - assign with the current
+                datetime when an instance is created
                 and it will be updated every time you change your object
         """
         self.id = str(uuid4())
@@ -22,11 +24,15 @@ class BaseModel:
     def __str__(self):
         """string representation of the class"""
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
     def save(self):
-        """ updates the public instance attribute updated_at with the current datetime"""
+        """ updates the public instance attribute updated_at
+            with the current datetime"""
         self.updated_at = datetime.now()
+
     def to_dict(self):
-        """ returns a dictionary containing all keys/values of __dict__ of the instance"""
+        """ returns a dictionary containing all keys/values
+            of __dict__ of the instance"""
         my_dict = self.__dict__.copy()
         my_dict['__class__'] = self.__class__.__name__
         my_dict['created_at'] = self.created_at.isoformat()
