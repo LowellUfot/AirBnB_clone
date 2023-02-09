80c71a6a65a19b3fedf3270167c4ad2957c622c3
LowellUfot <lowellufot@gmail.com>
2023-02-08 18:16:33 -0800

    update the test module. test_to_dict() still needs updating

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -5,7 +5,9 @@
 
 
 import unittest
-from models.BaseModel import BaseModel
+import json
+from datetime import datetime
+from models.base_model import BaseModel
 
 
 class TestBaseModel(unittest.TestCase):
@@ -19,12 +21,12 @@ class TestBaseModel(unittest.TestCase):
 
         self.assertIsInstance(bm1, BaseModel)
         self.assertIsInstance(bm2, BaseModel)
-        self.assetNotEqual(bm1.id, bm2.id)
+        self.assertNotEqual(bm1.id, bm2.id)
         self.assertIsInstance(bm1.id, str)
         self.assertIsInstance(bm2.id, str)
 
     def test_created_updated_at(self):
-        """test scenarion of type of created_at and updated_at"""
+        """test scenario of type of created_at and updated_at"""
         bm = BaseModel()
         self.assertIsInstance(bm.created_at, datetime)
         self.assertIsInstance(bm.updated_at, datetime)
@@ -36,18 +38,17 @@ class TestBaseModel(unittest.TestCase):
         """
         bm = BaseModel()
         time1 = bm.updated_at
-        bm.save
+        bm.save()
         time2 = bm.updated_at
-        self.assertIsNotEqual(time1, time2)
+        self.assertNotEqual(time1, time2)
 
     def test_to_dict(self):
         """test scenario for to_dict method
             checks if method returns json type
         """
         bm = BaseModel()
-        json_string = bm.to_dict
-        python_string = json.loads(json_string)
-        self.assertIsInstance(python_string, dict)
+        dict_rep = bm.to_dict
+        self.assertIsInstance(dict_rep, dict)
 
     if __name__ == '__main__':
         unittest.main()
